83c8f3421b554858d0b44fd2d6106e7b21808f87
tu-nv <vantu.bkhn@gmail.com>
2020-04-30 21:34:19 +0900

    install editable module using requirement files

--- install_reqs_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# install requirements for dev.
-#
-
-pip install -r ni_mon/requirements.txt
-pip install -r ni_nfvo/requirements.txt
-pip install -r kafka_extractor/requirements.txt
-./install_reqs_tests.sh
--- install_reqs_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# install requirements for dev.
-#
-
-pip install -e ni_mon_client_api
-pip install -e ni_nfvo_client_api
-pip install pytest
-pip install pyyaml
