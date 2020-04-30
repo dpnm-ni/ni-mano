78dcaffb38a34d40e375958f4325444739eda7b7
tu-nv <vantu.bkhn@gmail.com>
2020-04-30 18:25:53 +0900

    ni_mon: put topo info inside config file so that we have consistency config style in all ni_mano modules

--- ni_mon/config.py
+++ ni_mon/config.py
@@ -10,8 +10,4 @@ file_path = os.path.join(package_dir,"config/config.yaml")
 with open(file_path, 'r') as yaml_file:
     cfg = yaml.load(yaml_file, yaml.SafeLoader)
     cfg["kafka_client"]["consumer"]["group.id"] = "kafka_extractor_%s" %(uuid.uuid4())
-
-
-file_path = os.path.join(package_dir,"config/topo.yaml")
-with open(file_path, 'r') as yaml_file:
-    topo = yaml.load(yaml_file, yaml.SafeLoader)
+    topo = cfg["topo"]
